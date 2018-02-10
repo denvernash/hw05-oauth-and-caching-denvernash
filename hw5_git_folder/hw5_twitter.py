68b963c6714b7876a0a780e9519f28e253ff245f
denvernash <35231540+denvernash@users.noreply.github.com>
2018-02-09 22:09:41 -0500

    added CACHE function

--- hw5_git_folder/hw5_twitter.py
+++ hw5_git_folder/hw5_twitter.py
@@ -8,7 +8,7 @@ import secret_data # file that contains OAuth credentials
 
 ## SI 206 - HW
 ## COMMENT WITH:
-## Your section day/time:
+## Your section day/time: 005/ M 1-2:30
 ## Any names of people you worked with on this assignment:
 
 #usage should be python3 hw5_twitter.py <username> <num_tweets>
@@ -26,11 +26,53 @@ auth = OAuth1(consumer_key, consumer_secret, access_token, access_secret)
 requests.get(url, auth=auth)
 #Code for OAuth ends
 
+
 #Write your code below:
 #Code for Part 3:Caching
+
+# initializing CACHE dictionary
+CACHE_FNAME = "twitter_cache.json"
+try:
+    with open(CACHE_FNAME) as json_data:
+        CACHE_DICTION = json.loads(json_data)
+except:
+    CACHE_DICTION = {}
+
+def sorted_search_params(baseurl, auth, params):
+    sorted_params = sorted(params.keys())
+    acc = []
+    for item in sorted_params:
+        acc.append("{}-{}".format(item, params[item]))
+    return baseurl + "_".join(acc)
+
+def fetch_from_twitter(screen_name, count = 20):
+    baseurl = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+    auth = OAuth1(consumer_key, consumer_secret, access_token, access_secret)
+    params = {}
+    params["screen_name"] = screen_name
+    params["count"] = count
+    search_id = sorted_search_params(baseurl, auth = auth, params = params)
+    if search_id in CACHE_DICTION:
+        print("Returning data from cache file")
+        return CACHE_DICTION[search_id]
+    else:
+        response = requests.get(baseurl, auth= auth, params = params)
+        print("Getting fresh data from Twitter")
+        twitter_data = json.loads(response.text)
+        CACHE_DICTION[search_id] = twitter_data
+        fname = open(CACHE_FNAME, 'w')
+        fname.write(json.dumps(CACHE_DICTION))
+        fname.close()
+        return twitter_data
+
+
 #Finish parts 1 and 2 and then come back to this
 
-#Code for Part 1:Get Tweets
+# Code for Part 1:Get Tweets
+
+data = fetch_from_twitter(username, num_tweets)
+
+
 
 #Code for Part 2:Analyze Tweets
 
